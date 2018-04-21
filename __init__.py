3c11f986d60108630b743e0416ff7e9dbc6d52f7
fay111101 <1139598278@qq.com>
2018-04-21 01:54:04 -0700

    mlpractice

---
+++ __init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+"""
+    File Name: __init__.py
+    Description:
+    Author: fay
+    date: 09/04/18
+    ----------------------------
+    Change Activity:
+                    09/04/18
+    XXX is a module for XXX
+    :copyright: (c) Copyright 2017 by Xiuhong Fei.
+    :license: iscas, see LICENSE for more details.
+"""
+__author__ = 'fay'
---
+++ code/pca.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+"""
+    File Name: pca
+    Description:
+    Author: fay
+    date: 06/04/18
+    ----------------------------
+    Change Activity:
+                    06/04/18
+    XXX is a module for XXX
+    :copyright: (c) Copyright 2017 by Xiuhong Fei.
+    :license: iscas, see LICENSE for more details.
+"""
+__author__ = 'fay'
